fe69736684d66249dc837c20c5cc6e914f2cee35
rishflab <rishflab@hotmail.com>
2021-03-11 10:36:18 +1100

    Split setup and main

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -13,69 +13,14 @@ use bitcoin_harness::BitcoindRpcApi;
 use hyper::body::Buf;
 use url::Url;
 
-use crate::bdk_wallet::BdkWallet;
 use bdk::FeeRate;
 use std::str::FromStr;
-
-mod bdk_wallet;
 
 const BITCOIND_RPC_PORT: u16 = 7041;
 const ELECTRUM_RPC_PORT: u16 = 60401;
 const ELECTRUM_HTTP_PORT: u16 = 3012;
 const USERNAME: &str = "admin";
 const PASSWORD: &str = "123";
-
-/// Create a test wallet, generate enough block to fund it and activate segwit.
-/// Generate enough blocks to make the passed `spendable_quantity` spendable.
-/// Spawn a tokio thread to mine a new block every second.
-pub async fn init_wallet(spendable_quantity: u32, wallet_name: &str, node_url: Url) -> Result<()> {
-    println!("node url: {}", node_url);
-
-    let bitcoind_client = bitcoin_harness::Client::new(node_url.clone());
-
-    bitcoind_client
-        .createwallet(&wallet_name, None, None, None, None)
-        .await?;
-
-    let reward_address = bitcoind_client
-        .with_wallet(wallet_name)?
-        .getnewaddress(None, None)
-        .await?;
-
-    bitcoind_client
-        .generatetoaddress(101 + spendable_quantity, reward_address.clone(), None)
-        .await?;
-    let _ = tokio::spawn(mine(bitcoind_client, reward_address));
-
-    Ok(())
-}
-
-async fn mine(
-    bitcoind_client: bitcoin_harness::Client,
-    reward_address: bitcoin::Address,
-) -> Result<()> {
-    loop {
-        tokio::time::delay_for(Duration::from_secs(1)).await;
-        bitcoind_client
-            .generatetoaddress(1, reward_address.clone(), None)
-            .await?;
-    }
-}
-
-#[tokio::main]
-async fn setup() {
-    // note: you need to generate 101 blocks first
-    // note2: miner wallet name is `miner`
-    let bitcoind_url = {
-        let input = format!(
-            "http://{}:{}@localhost:{}",
-            USERNAME, PASSWORD, BITCOIND_RPC_PORT
-        );
-        Url::parse(&input).unwrap()
-    };
-
-    init_wallet(1, "miner", bitcoind_url).await.unwrap();
-}
 
 #[tokio::main]
 async fn main() {
