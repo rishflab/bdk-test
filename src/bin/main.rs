c4e3d5bed82e5324d78871a31ea1cc81b2cd0737
rishflab <rishflab@hotmail.com>
2021-03-11 10:54:59 +1100

    WIP:removing unneeded code

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,20 +1,20 @@
-use std::sync::Arc;
-use std::time::Duration;
-
 use anyhow::Result;
 use bdk::blockchain::{noop_progress, ConfigurableBlockchain, LogProgress};
 use bdk::blockchain::{Blockchain, ElectrumBlockchain};
 use bdk::database::MemoryDatabase;
 use bdk::electrum_client::{Client, ElectrumApi};
+use bdk::FeeRate;
 use bitcoin::consensus::serialize;
 use bitcoin::util::psbt::PartiallySignedTransaction;
 use bitcoin::{Amount, Network, Transaction};
 use bitcoin_harness::BitcoindRpcApi;
 use hyper::body::Buf;
+use std::str::FromStr;
+use std::sync::Arc;
+use std::time::Duration;
 use url::Url;
 
-use bdk::FeeRate;
-use std::str::FromStr;
+use bdk_test::Wallet;
 
 const BITCOIND_RPC_PORT: u16 = 7041;
 const ELECTRUM_RPC_PORT: u16 = 60401;
@@ -45,41 +45,24 @@ async fn main() {
         Url::parse(&input).unwrap()
     };
 
-    let client = bdk::electrum_client::Client::new(bdk_url.as_str()).unwrap();
+    let bdk_wallet = Wallet::new(&bdk_url).await?;
 
-    let blockchain = ElectrumBlockchain::from(client);
-
-    let bdk_wallet = bdk::Wallet::new(
-        "wpkh(tprv8ZgxMBicQKsPdpkqS7Eair4YxjcuuvDPNYmKX3sCniCf16tHEVrjjiSXEkFRnUH77yXc6ZcwHHcLNfjdi5qUvw3VDfgYiH5mNsj5izuiu2N/0/0/*)",
-        None,
-        Network::Regtest,
-        MemoryDatabase::default(),
-        blockchain).unwrap();
-
-    let address = bdk_wallet.get_new_address().unwrap();
-    println!("funded address: {}", address);
-
-    // sync wallet
-    bdk_wallet.sync(noop_progress(), None).unwrap();
-
-    // subscribe to block headers
-    let client = bdk::electrum_client::Client::new(bdk_url.as_str()).unwrap();
-    client.block_headers_subscribe().unwrap();
+    bdk_wallet.sync().await.unwrap();
 
     // get some money for our bdk wallet if n balance
-    while bdk_wallet.get_balance().unwrap() <= 200_000 {
+    while bdk_wallet.balance().await.unwrap() <= 200_000 {
         miner
             .send_to_address("miner", address.clone(), Amount::from_sat(100_000_000))
             .await
             .unwrap();
 
-        bdk_wallet.sync(noop_progress(), None).unwrap();
+        bdk_wallet.sync().await.unwrap();
     }
 
-    let balance = bdk_wallet.get_balance().unwrap();
+    let balance = bdk_wallet.balance().await.unwrap();
     println!("balance: {}", balance);
 
-    let address = bdk_wallet.get_new_address().unwrap();
+    let address = bdk_wallet.new_address().await.unwrap();
     println!("target address: {}", address);
 
     let (psbt, details) = bdk_wallet
@@ -104,6 +87,4 @@ async fn main() {
 
     // update to get the latest 2 blocks
     bdk_wallet.sync(noop_progress(), None).unwrap();
-    let block_header_new = bdk_wallet.client().get_height().unwrap();
-    println!("New block height: {} ", block_header_new);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,184 +1,86 @@
-use std::time::Duration;
-use ::bitcoin::{Txid, util::psbt::PartiallySignedTransaction};
+use ::bitcoin::{util::psbt::PartiallySignedTransaction, Txid};
 use anyhow::Result;
+use bdk::blockchain::Blockchain;
 use bdk::{
     bitcoin::Network,
-    blockchain::{ElectrumBlockchain, noop_progress},
+    blockchain::{noop_progress, ElectrumBlockchain},
     database::MemoryDatabase,
     electrum_client::{Client, ElectrumApi},
     FeeRate,
 };
-use bdk::blockchain::{Blockchain};
 use bitcoin::{Address, Amount, Transaction};
 use reqwest::Method;
+use std::sync::{Arc, Mutex};
+use std::time::Duration;
 use tokio::time::interval;
 use url::Url;
 
-pub struct BdkWallet {
-    pub inner: bdk::Wallet<ElectrumBlockchain, MemoryDatabase>,
-    //pub client: Client,
+pub struct Wallet {
+    inner: Arc<Mutex<bdk::Wallet<ElectrumBlockchain, MemoryDatabase>>>,
 }
 
-impl BdkWallet {
-    // pub async fn new_alice( url: &Url, network: Network) -> Result<Self> {
-    //     match Client::new(url.as_str()) {
-    //         Ok(client) => {
-    //             let bdk_wallet = bdk::Wallet::new(
-    //                 "wpkh([c258d2e4/84h/1h/0h]tpubDDYkZojQFQjht8Tm4jsS3iuEmKjTiEGjG6KnuFNKKJb5A6ZUCUZKdvLdSDWofKi4ToRCwb9poe1XdqfUnP4jaJjCB2Zwv11ZLgSbnZSNecE/0/*)",
-    //                 Some("wpkh([c258d2e4/84h/1h/0h]tpubDDYkZojQFQjht8Tm4jsS3iuEmKjTiEGjG6KnuFNKKJb5A6ZUCUZKdvLdSDWofKi4ToRCwb9poe1XdqfUnP4jaJjCB2Zwv11ZLgSbnZSNecE/1/*)"),
-    //                 bitcoin::Network::Regtest,
-    //                 MemoryDatabase::default(),
-    //                 ElectrumBlockchain::from(client)
-    //             )?;
-    //
-    //             Ok(Self {
-    //                 inner: bdk_wallet,
-    //                 //client: client2,
-    //             })
-    //         }
-    //         Err(err) => {
-    //             println!("{:?}", err);
-    //             Err(anyhow::Error::msg("could not init bdk wallet"))
-    //         },
-    //     }
-    // }
+impl Wallet {
+    pub async fn new(url: &Url) -> Result<Self> {
+        let client = bdk::electrum_client::Client::new(url.as_str()).unwrap();
 
-    // pub async fn new_bob(client: Client, network: Network) -> Result<Self> {
-    //
-    //
-    //     let bdk_wallet = bdk::Wallet::new(
-    //         "wpkh([c258d2e4/84h/1h/0h]tpubDDYkZojQFQjht8Tm4jsS3iuEmKjTiEGjG6KnuFNKKJb5A6ZUCUZKdvLdSDWofKi4ToRCwb9poe1XdqfUnP4jaJjCB2Zwv11ZLgSbnZSNecE/0/*)",
-    //         Some("wpkh([c258d2e4/84h/1h/0h]tpubDDYkZojQFQjht8Tm4jsS3iuEmKjTiEGjG6KnuFNKKJb5A6ZUCUZKdvLdSDWofKi4ToRCwb9poe1XdqfUnP4jaJjCB2Zwv11ZLgSbnZSNecE/1/*)"),
-    //         network,
-    //         MemoryDatabase::default(),
-    //         ElectrumBlockchain::from(client)
-    //     )?;
-    //
-    //     Ok(Self {
-    //         inner: bdk_wallet,
-    //         //client: client2,
-    //     })
-    //
-    //
-    // }
+        let header_notif = client.block_headers_subscribe().unwrap();
+        dbg!(header_notif);
 
+        let blockchain = ElectrumBlockchain::from(client);
 
+        let bdk_wallet = bdk::Wallet::new(
+            "wpkh(tprv8ZgxMBicQKsPdpkqS7Eair4YxjcuuvDPNYmKX3sCniCf16tHEVrjjiSXEkFRnUH77yXc6ZcwHHcLNfjdi5qUvw3VDfgYiH5mNsj5izuiu2N/0/0/*)",
+            None,
+            Network::Regtest,
+            MemoryDatabase::default(),
+            blockchain).unwrap();
 
-    pub fn get_network(&self) -> bitcoin::Network {
-        self.inner.network()
+        Ok(Self {
+            inner: Arc::new(Mutex::new(bdk_wallet)),
+        })
+    }
+
+    pub async fn balance(&self) -> Result<Amount> {
+        let balance = self
+            .inner
+            .lock()
+            .await
+            .get_balance()
+            .context("Failed to calculate Bitcoin balance")?;
+
+        Ok(Amount::from_sat(balance))
+    }
+
+    pub async fn get_block_height(&self, electrum_http_url: Url) -> String {
+        let client = reqwest::Client::new();
+        let resp = client
+            .request(Method::GET, electrum_http_url)
+            .send()
+            .await
+            .unwrap();
+        let height = resp.text().await.unwrap();
+
+        height
+    }
+
+    pub async fn new_address(&self) -> Result<Address> {
+        let address = self
+            .inner
+            .lock()
+            .await
+            .get_new_address()
+            .context("Failed to get new Bitcoin address")?;
+
+        Ok(address)
+    }
+
+    pub async fn sync(&self) -> Result<()> {
+        self.inner
+            .lock()
+            .await
+            .sync(noop_progress(), None)
+            .context("Failed to sync balance of Bitcoin wallet")?;
+
+        Ok(())
     }
 }
-
-pub async fn build_tx_lock_psbt(
-    wallet: &bdk::Wallet<ElectrumBlockchain, MemoryDatabase>,
-    output_address: Address,
-    output_amount: Amount,
-) -> Result<PartiallySignedTransaction> {
-
-    // let (psbt, _details) = wallet.create_tx(
-    //     bdk::TxBuilder::with_recipients(vec![(
-    //         output_address.script_pubkey(),
-    //         output_amount.as_sat()
-    //     )])
-    //         // .enable_rbf()
-    //         // .do_not_spend_change()
-    //         // .fee_rate(FeeRate::from_sat_per_vb(5.0)),
-    // )?;
-
-
-    let (psbt, _details) = wallet.create_tx(
-        bdk::TxBuilder::with_recipients(vec![(output_address.script_pubkey(), output_amount.as_sat())])
-            .enable_rbf()
-            .do_not_spend_change()
-            .fee_rate(FeeRate::from_sat_per_vb(5.0))
-    )?;
-
-
-    Ok(psbt)
-}
-
-pub async fn get_block_height(electrum_http_url: Url) -> String {
-
-    let client = reqwest::Client::new();
-    let resp = client.request(Method::GET, electrum_http_url).send().await.unwrap();
-    let height = resp.text().await.unwrap();
-
-    height
-}
-
-// async fn sign_tx_lock( wallet: &bdk::Wallet<ElectrumBlockchain, MemoryDatabase>, tx_lock: TxLock) -> Result<Transaction> {
-//     let psbt = PartiallySignedTransaction::from(tx_lock);
-//
-//     let (signed_psbt, _finalized) = wallet.sign(psbt, None)?;
-//
-//     let tx = signed_psbt.extract_tx();
-//
-//     Ok(tx)
-// }
-
-
-// #[async_trait]
-// impl BroadcastSignedTransaction for BdkWallet {
-//     async fn broadcast_signed_transaction(&self, transaction: Transaction) -> Result<Txid> {
-//         let txid = self.inner.broadcast(transaction)?;
-//         tracing::info!("Bitcoin tx broadcasted! TXID = {}", txid);
-//         Ok(txid)
-//     }
-// }
-//
-// // TODO: For retry, use `backoff::ExponentialBackoff` in production as opposed
-// // to `ConstantBackoff`.
-// #[async_trait]
-// impl WatchForRawTransaction for BdkWallet {
-//     async fn watch_for_raw_transaction(&self, txid: Txid) -> Transaction {
-//         (|| async { Ok(self.get_raw_transaction(txid).await?) })
-//             .retry(ConstantBackoff::new(Duration::from_secs(1)))
-//             .await
-//             .expect("transient errors to be retried")
-//     }
-// }
-//
-// #[async_trait]
-// impl GetRawTransaction for BdkWallet {
-//     // todo: potentially replace with option
-//     async fn get_raw_transaction(&self, txid: Txid) -> Result<Transaction> {
-//         let tx = self.inner.client().unwrap().get_tx(&txid)?;
-//         Ok(tx.unwrap())
-//     }
-// }
-//
-
-
-// #[async_trait]
-// impl TransactionBlockHeight for BdkWallet {
-//     async fn transaction_block_height(&self, txid: Txid) -> BlockHeight {
-//         let merkle_res = self.client.block_header();
-//         let height = merkle_res.block_height;
-//
-//         BlockHeight::new(height as u32)
-//     }
-// }
-//
-// #[async_trait]
-// impl WaitForTransactionFinality for BdkWallet {
-//     async fn wait_for_transaction_finality(&self, txid: Txid, config: Config) -> Result<()> {
-//         // TODO(Franck): This assumes that bitcoind runs with txindex=1
-//
-//         // Divide by 4 to not check too often yet still be aware of the new block early
-//         // on.
-//         let mut interval = interval(config.bitcoin_avg_block_time / 4);
-//
-//         loop {
-//             let tx = self.get_raw_transaction(txid).await?;
-//             if let Some(confirmations) = tx.confirmations {
-//                 if confirmations >= config.bitcoin_finality_confirmations {
-//                     break;
-//                 }
-//             }
-//             interval.tick().await;
-//         }
-//
-//         Ok(())
-//     }
-// }
-//
